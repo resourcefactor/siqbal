170a4c7500022972b1e8d6bfa6a746d7c303a291
smehta <bhagwatmehta779@gmail.com>
2022-03-01 15:42:49 +0530

    updated in payment entry sales order ref

--- siqbal/hook_events/payment_entry.py
+++ siqbal/hook_events/payment_entry.py
@@ -6,12 +6,15 @@ from frappe import _
 def validate_sales_order(pe, method):
 	for reference in pe.references:
 		if reference.reference_doctype in ["Sales Invoice", "Sales Order"]:
+			so = None
 			if reference.reference_doctype == "Sales Invoice":
 				so = frappe.db.get_value("Sales Invoice", reference.reference_name, "cust_sales_order_number")
 			elif reference.reference_doctype == "Sales Order":
 				so = reference.reference_name
 			if reference.sales_order != so:
 				reference.sales_order = so
+				if method == 'on_update_after_submit':
+					reference.db_update()
 
 
 # def validate_salaryslip_amount(pe, method):
