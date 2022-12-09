da07952f64c72db1d64d897b2df237569dc608a3
hamzaali15 <malikhamzaali48@gmail.com>
2022-12-09 10:58:48 +0500

    cust: changes updated for user level warehouse & costcenter

--- siqbal/hooks.py
+++ siqbal/hooks.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from . import __version__ as app_version
 
 app_name = "SIqbal"
 app_title = "SIqbal"
@@ -32,30 +31,30 @@ app_include_css = "/assets/siqbal/css/siqbal.css"
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
 doctype_js = {
-		"Address": "public/js/address.js",
-		"Architect and Contractor": "public/js/architect_and_contractor.js",
-		"Authorization Rule": "public/js/authorization_rule.js",
-		"Customer": "public/js/customer.js",
-		"Delivery Note" : "public/js/delivery_note.js",
-		"Item": "public/js/item.js",
-		"Journal Entry": "public/js/journal_entry.js",
-		"Landed Cost Voucher": "public/js/landed_cost_voucher.js",
-		"Material Request" : "public/js/material_request.js",
-		"Opportunity": "public/js/opportunity.js",
-		"Payment Entry": "public/js/payment_entry.js",
-		"Property Detail": "public/js/property_detail.js",
-		"Purchase Invoice" : "public/js/purchase_invoice.js",
-		"Purchase Order" : "public/js/purchase_order.js",
-		"Purchase Receipt" : "public/js/purchase_receipt.js",
-		"Quotation" : "public/js/quotation.js",
-		"Request for Quotation": "public/js/request_for_quotation.js",
-		"Salary Slip" : "public/js/salary_slip.js",
-		"Sales Invoice" : "public/js/sales_invoice.js",
-		"Sales Order" : "public/js/sales_order.js",
-		"Stock Entry" : "public/js/stock_entry.js",
-		"Stock Reconciliation" : "public/js/stock_reconciliation.js",
-		"Supplier Quotation": "public/js/supplier_quotation.js"
-	}
+	"Address": "public/js/address.js",
+	"Architect and Contractor": "public/js/architect_and_contractor.js",
+	"Authorization Rule": "public/js/authorization_rule.js",
+	"Customer": "public/js/customer.js",
+	"Delivery Note": "public/js/delivery_note.js",
+	"Item": "public/js/item.js",
+	"Journal Entry": "public/js/journal_entry.js",
+	"Landed Cost Voucher": "public/js/landed_cost_voucher.js",
+	"Material Request": "public/js/material_request.js",
+	"Opportunity": "public/js/opportunity.js",
+	"Payment Entry": "public/js/payment_entry.js",
+	"Property Detail": "public/js/property_detail.js",
+	"Purchase Invoice": "public/js/purchase_invoice.js",
+	"Purchase Order": "public/js/purchase_order.js",
+	"Purchase Receipt": "public/js/purchase_receipt.js",
+	"Quotation": "public/js/quotation.js",
+	"Request for Quotation": "public/js/request_for_quotation.js",
+	"Salary Slip": "public/js/salary_slip.js",
+	"Sales Invoice": "public/js/sales_invoice.js",
+	"Sales Order": "public/js/sales_order.js",
+	"Stock Entry": "public/js/stock_entry.js",
+	"Stock Reconciliation": "public/js/stock_reconciliation.js",
+	"Supplier Quotation": "public/js/supplier_quotation.js"
+}
 
 # Home Pages
 # ----------
@@ -65,7 +64,7 @@ doctype_js = {
 
 # website user home page (by Role)
 # role_home_page = {
-#	"Role": "home_page"
+# 	"Role": "home_page"
 # }
 
 # Website user home page (by function)
@@ -110,17 +109,12 @@ doctype_js = {
 # 		"on_update": "method",
 # 		"on_cancel": "method",
 # 		"on_trash": "method"
-#	}
+# 	}
 # }
 
 doc_events = {
 	"Sales Order": {
-		"validate": [
-			"siqbal.hook_events.sales_order.set_average_valuation_rate",
-		#	"siqbal.utils.validate_date"
-		],
-		"before_submit": "siqbal.hook_events.sales_order.unset_needs_approval",
-		"before_update_after_submit": "siqbal.hook_events.sales_order.validate_items_rate_and_update_boxes"
+		"validate": "siqbal.hook_events.sales_order.set_average_valuation_rate"
 	},
 	"Sales Invoice": {
 		"validate": [
@@ -128,58 +122,31 @@ doc_events = {
 			"siqbal.hook_events.sales_invoice.validate_taxes_and_charges_from_so",
 			"siqbal.utils.add_location",
 			"siqbal.hook_events.sales_invoice.validate_sales_invoice"
-		#	"siqbal.utils.validate_date"
 		],
 		"before_insert": "siqbal.hook_events.sales_invoice.set_supplier_details",
 		"on_submit": [
 			"siqbal.hook_events.sales_invoice.update_reserved_qty",
 			"siqbal.hook_events.sales_invoice.create_purchase_invoices_against_sales_taxes",
-			# "siqbal.utils.change_pi_status"			
-			#"siqbal.hook_events.sales_invoice.validate_user_warehouse"
+			"siqbal.hook_events.sales_invoice.validate_user_warehouse"
 		],
 		"on_cancel": "siqbal.hook_events.sales_invoice.update_reserved_qty"
 	},
 	"Payment Entry": {
-		"validate": [
-			"siqbal.hook_events.payment_entry.validate_sales_order",
-			# "siqbal.hook_events.payment_entry.validate_salaryslip_amount",
-			#"siqbal.utils.validate_date"
-		],
+		"validate": "siqbal.hook_events.payment_entry.validate_sales_order",
 		"on_update_after_submit": "siqbal.hook_events.payment_entry.validate_sales_order"
-		# "on_submit": "siqbal.hook_events.payment_entry.update_salaryslip_status",
-		# "on_cancel": "siqbal.hook_events.payment_entry.update_salaryslip_status"
 	},
 	"Stock Entry": {
-		#"validate": "siqbal.utils.validate_date",
-		#"on_submit": "siqbal.hook_events.stock_entry.validate_user_warehouse"
-	},
-	"Opportunity": {
-		"validate": "siqbal.utils.send_followup_sms"
+		"on_submit": "siqbal.hook_events.stock_entry.validate_user_warehouse"
 	},
 	"Purchase Invoice": {
 		"validate": "siqbal.utils.add_location"
-	},
-	"Purchase Order": {
-		#"validate": "siqbal.utils.validate_date"
-	},
-	"Purchase Receipt": {
-		#"validate": "siqbal.utils.validate_date"
-	},
-	"Stock Reconciliation": {
-		#"validate": "siqbal.utils.validate_date"
-	},
-	# "Quotation": {
-		#"validate": "siqbal.utils.validate_date"
-	# },
-	# "Journal Entry": {
-	# 	"before_save": "siqbal.hook_events.journal_entry.set_name"
-	# }
+	}
 }
 
 
 jenv = {
-	"methods" : [
-	"get_qrcode_image:siqbal.utils.get_qrcode_image"
+	"methods": [
+		"get_qrcode_image:siqbal.utils.get_qrcode_image"
 	]
 }
 
@@ -228,4 +195,3 @@ override_doctype_class = {
 	'Sales Invoice': 'siqbal.hook_events.overide_sales_invoice.OverrideSalesInvoice',
 	'Purchase Invoice': 'siqbal.hook_events.override_purchase_invoice.OverridePurchaseInvoice'
 }
-
