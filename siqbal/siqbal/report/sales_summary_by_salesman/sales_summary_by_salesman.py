ef8fe2457c39c80e7d0de352a06dd5c28d3820be
Hamza Ali <hamza.a@youpalgroup.com>
2023-05-09 13:20:13 +0500

    cust: Sales Summary By Salesman report updated

--- siqbal/siqbal/report/sales_summary_by_salesman/sales_summary_by_salesman.py
+++ siqbal/siqbal/report/sales_summary_by_salesman/sales_summary_by_salesman.py
@@ -21,7 +21,7 @@ def get_data(filters):
 		ifnull(canceld_sale,0),
 		ifnull(sales_return,0),
 		ifnull(old_system_return,0),
-		(ifnull(rounded_total,0)-ifnull(canceld_sale,0)-ifnull(sales_return,0)-ifnull(old_system_return,0)),
+		(ifnull(rounded_total,0)-ifnull(sales_return,0)-ifnull(old_system_return,0)),
 		ifnull(cash_sale,0),
 		ifnull(credit_sale,0),
 		ifnull(Recovery,0)
@@ -32,7 +32,7 @@ def get_data(filters):
 		sum(if(customer_group="Cash Customer",rounded_total,0)) as cash_sale,
 		sum(if(customer_group="Credit Customer",rounded_total,0)) as credit_sale,
 		owner
-		from `tabSales Order` where docstatus>=1 and company= %(company)s and transaction_date>=%(fdate)s and transaction_date<=%(tdate)s group by owner)
+		from `tabSales Order` where docstatus=1 and company= %(company)s and transaction_date>=%(fdate)s and transaction_date<=%(tdate)s group by owner)
 
 		tso on tsm.salesman = tso.owner left join
 
@@ -40,7 +40,7 @@ def get_data(filters):
 
 		left join
 
-		(select tc.cost_center,sales_order_owner, abs(sum(tsi.net_amount)) as sales_return from `tabSales Invoice` tc inner join `tabSales Invoice Item` tsi on tc.name=tsi.parent where is_return=1 and tc.docstatus=1 and company= %(company)s and tc.posting_date>=%(fdate)s and tc.posting_date<=%(tdate)s group by tc.sales_order_owner) tsr
+		(select ts.cost_center,sales_order_owner, abs(sum(tsi.net_amount)) as sales_return from `tabSales Invoice` ts inner join `tabSales Invoice Item` tsi on ts.name=tsi.parent where is_return=1 and ts.docstatus=1 and company= %(company)s and ts.posting_date>=%(fdate)s and ts.posting_date<=%(tdate)s group by ts.sales_order_owner) tsr
 
 		on tsm.salesman=tsr.sales_order_owner left join
 
