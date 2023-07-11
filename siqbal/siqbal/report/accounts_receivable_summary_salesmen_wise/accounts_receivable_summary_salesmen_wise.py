4917ac3d8866718c16c1dc38cdb6e1c48ed242c9
hamzaali15 <malikhamzaali48@gmail.com>
2023-07-11 17:12:47 +0500

    Accounts Receivable Summary Salesmen Wise report completed

--- siqbal/siqbal/report/accounts_receivable_summary_salesmen_wise/accounts_receivable_summary_salesmen_wise.py
+++ siqbal/siqbal/report/accounts_receivable_summary_salesmen_wise/accounts_receivable_summary_salesmen_wise.py
@@ -5,10 +5,12 @@
 from __future__ import unicode_literals
 import frappe
 
+
 def execute(filters=None):
 	columns = get_columns()
 	data = get_data(filters)
 	return columns, data
+
 
 def get_columns():
 	columns = [
@@ -63,28 +65,26 @@ def get_columns():
 	]
 	return columns
 
+
 def get_data(filters):
 	query = """select
 			so.customer_name,
 			sum(so.grand_total) as sales_order_amount,
-			
+
 			(select sum(si.grand_total)
 			from `tabSales Invoice` as si
 			inner join `tabSales Invoice Item` as sii on sii.parent=si.name and sii.sales_order=so.name
 			where si.sales_order_owner=so.owner and si.customer=so.customer and si.docstatus=1) as sales_invoice_amount,
-			
+
 			(select sum(si.grand_total)
 			from `tabSales Invoice` as si
 			inner join `tabSales Invoice Item` as sii on sii.parent=si.name and sii.sales_order=so.name
 			where si.sales_order_owner=so.owner and si.customer=so.customer and si.docstatus=1 and si.is_return=1) as sales_return,
-			
+
 			(select sum(per.total_amount)
 			from `tabPayment Entry` as pe
 			inner join `tabPayment Entry Reference` as per on pe.name=per.parent and so.name=per.sales_order
-			and pe.docstatus=1) as payment_received,
-			
-			0 as balance_per_so,
-			0 as balance_per_si
+			and pe.docstatus=1) as payment_received
 
 			from `tabSales Order` as so
 			where so.docstatus = 1 and so.company = '{0}'
@@ -109,8 +109,8 @@ def get_data(filters):
 			"pending_sales_order_amount": pending_sales_order_amount,
 			"sales_return": row.sales_return,
 			"payment_received": row.payment_received,
-			"balance_per_so": row.balance_per_so,
-			"balance_per_si": row.balance_per_si
+			"balance_per_so": (row.sales_order_amount if row.sales_order_amount else 0) - (row.sales_return if row.sales_return else 0) - (row.payment_received if row.payment_received else 0),
+			"balance_per_si": (row.sales_invoice_amount if row.sales_invoice_amount else 0) - (row.sales_return if row.sales_return else 0) - (row.payment_received if row.payment_received else 0)
 		}
 		data.append(row)
 
