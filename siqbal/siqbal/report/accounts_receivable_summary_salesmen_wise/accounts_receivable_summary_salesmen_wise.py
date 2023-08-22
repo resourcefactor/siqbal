2d3c5fd397e6b8d0b0ca65d5e1d7872b1da3b86e
hamzaali15 <malikhamzaali48@gmail.com>
2023-08-22 14:05:52 +0500

    cust: Accounts Receivable Summary Salesmen Wise report updated

--- siqbal/siqbal/report/accounts_receivable_summary_salesmen_wise/accounts_receivable_summary_salesmen_wise.py
+++ siqbal/siqbal/report/accounts_receivable_summary_salesmen_wise/accounts_receivable_summary_salesmen_wise.py
@@ -72,7 +72,7 @@ def get_data(filters):
 		salesman_query += " and so.owner = '{0}'".format(filters.get('salesman'))
 
 	query = """select foo.customer_name, sum(foo.sales_order_amount) as sales_order_amount,
-		sum(sales_invoice_amount) + invoice_tax_amount - additional_discount_amount as sales_invoice_amount, sum(sales_return) + return_tax_amount as sales_return,
+		sum(sales_invoice_amount) + invoice_tax_amount - additional_discount_amount as sales_invoice_amount, sum(sales_return) - return_tax_amount - return_additional_discount_amount as sales_return,
 		sum(payment_received) as payment_received
 		from
 			(select so.customer_name, so.grand_total as sales_order_amount,
@@ -84,7 +84,7 @@ def get_data(filters):
 
 			(select ifnull(sum(si.total_taxes_and_charges), 0)
 			from `tabSales Invoice` as si
-			where si.sales_order_owner=so.owner and si.customer=so.customer and si.docstatus=1) as invoice_tax_amount,
+			where si.sales_order_owner=so.owner and si.customer=so.customer and si.docstatus=1 and si.is_return=0) as invoice_tax_amount,
 
 			(select ifnull(sum(si.discount_amount), 0)
 			from `tabSales Invoice` as si
@@ -98,6 +98,10 @@ def get_data(filters):
 			(select ifnull(sum(si.total_taxes_and_charges), 0)
 			from `tabSales Invoice` as si
 			where si.sales_order_owner=so.owner and si.customer=so.customer and si.docstatus=1 and si.is_return=1) as return_tax_amount,
+
+			(select ifnull(sum(si.discount_amount), 0)
+			from `tabSales Invoice` as si
+			where si.sales_order_owner=so.owner and si.customer=so.customer and si.docstatus=1 and si.is_return=1) as return_additional_discount_amount,
 
 			(select ifnull(sum(per.allocated_amount), 0)
 			from `tabPayment Entry Reference` as per
