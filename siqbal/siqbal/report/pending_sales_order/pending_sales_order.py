7070344025de0aeab9629aa457cc8ba09d65c249
hamzaali15 <malikhamzaali48@gmail.com>
2024-01-29 18:45:20 +0500

    pending sales order report's query updated

--- siqbal/siqbal/report/pending_sales_order/pending_sales_order.py
+++ siqbal/siqbal/report/pending_sales_order/pending_sales_order.py
@@ -12,7 +12,7 @@ def execute(filters=None):
 	if not (filters.get("customer_group")):
 		frappe.throw(_("Please set Customer Group First"))
 	else:
-		customer_filter = """ and customer_group in (select name from `tabCustomer Group`
+		customer_filter = """ and so.customer_group in (select name from `tabCustomer Group`
 						where lft>=(select lft from `tabCustomer Group` where name = '{0}')
 			and rgt<=(select rgt from `tabCustomer Group` where name = '{0}' ))""".format(filters.get("customer_group"))
 
@@ -26,7 +26,7 @@ def execute(filters=None):
 		{"fieldname": "total", "label": _("Total"), "fieldtype": "Currency", "width": 100},
 		{"fieldname": "payment", "label": _("Payment"), "fieldtype": "Currency", "width": 100},
 		{"fieldname": "returns", "label": _("Returns"), "fieldtype": "Currency", "width": 100},
-	#	{"fieldname": "return_payment", _("label"): "Returns Payment", "fieldtype": "Currency", "width": 100},
+		# {"fieldname": "return_payment", _("label"): "Returns Payment", "fieldtype": "Currency", "width": 100},
 		{"fieldname": "pending", "label": _("Pending"), "fieldtype": "Currency", "width": 100},
 		{"fieldname": "status", "label": _("Status"), "fieldtype": "Data", "width": 100},
 		{"fieldname": "delivery_status", "label": _("Delivery"), "fieldtype": "Data", "width": 100},
@@ -54,15 +54,15 @@ def execute(filters=None):
 
 	sales_orders = frappe.db.sql(sql_query, filters, as_dict=1)
 
-	gl_entries = frappe.db.sql("""
+	gl_entries_query = """
 		select
 			so.name as sales_order, gle.voucher_type, gle.against_voucher_type,
 			gle.against_voucher, gle.credit-gle.debit as amount
 		from `tabGL Entry` gle
 		inner join `tabSales Order` so
 			on (gle.against_voucher_type='Sales Order' and gle.against_voucher=so.name)
-		where voucher_no != against_voucher and so.transaction_date between %(fdate)s and %(tdate)s
-		and so.company = %(company)s and so.docstatus=1 and so.status not in ('Closed') {0} 
+		where voucher_no != against_voucher and so.transaction_date between '{0}' and '{1}'
+		and so.company = '{2}' and so.docstatus=1 and so.status not in ('Closed') {3}
 		and gle.is_cancelled = 0
 
 		union all
@@ -76,10 +76,13 @@ def execute(filters=None):
 			and gle.against_voucher in
 			(select distinct si.parent from `tabSales Invoice Item` si where si.sales_order=so.name )
 		)
-		where voucher_no != against_voucher  and so.transaction_date between %(fdate)s and %(tdate)s
-		and so.company = %(company)s and so.docstatus=1 and so.status not in ('Closed') {0} 
+		where voucher_no != against_voucher  and so.transaction_date between '{0}' and '{1}'
+		and so.company = '{2}' and so.docstatus=1 and so.status not in ('Closed') {3}
 		and gle.is_cancelled = 0
-	""".format(customer_filter), filters, as_dict=1)
+	""".format(filters.fdate, filters.tdate, filters.company, customer_filter)
+	print(gl_entries_query)
+
+	gl_entries = frappe.db.sql(gl_entries_query, as_dict=1)
 	order_gle = {}
 	for gle in gl_entries:
 		order_gle.setdefault(gle.sales_order, [])
@@ -97,9 +100,8 @@ def execute(filters=None):
 			else:
 				so.payment += gle.amount
 
-		so.pending = so.total - so.payment - so.returns  + so.return_payment
-		if so.pending > 1 :
+		so.pending = so.total - so.payment - so.returns + so.return_payment
+		if so.pending > 1:
 			data.append(so)
-
 
 	return columns, data
