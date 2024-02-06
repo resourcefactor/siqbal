56d9785daa8c5e51ab663511faa6220609109ae1
hamzaali15 <malikhamzaali48@gmail.com>
2024-02-06 23:30:43 +0500

    cust: salesmans pending sales orders report updated

--- siqbal/siqbal/report/salesmans_pending_sales_orders/salesmans_pending_sales_orders.py
+++ siqbal/siqbal/report/salesmans_pending_sales_orders/salesmans_pending_sales_orders.py
@@ -6,7 +6,6 @@ import frappe
 from frappe import _
 
 
-
 def execute(filters=None):
 	user = frappe.session.user
 	filters["user"] = user
@@ -14,11 +13,10 @@ def execute(filters=None):
 	if not (filters.get("customer_group")):
 		frappe.throw(_("Please set Customer Group First"))
 	else:
-		customer_filter = """ and customer_group in (select name from `tabCustomer Group`
+		customer_filter = """ and so.customer_group in (select name from `tabCustomer Group`
 						where lft>=(select lft from `tabCustomer Group` where name = '{0}')
 			and rgt<=(select rgt from `tabCustomer Group` where name = '{0}' ))""".format(filters.get("customer_group"))
 
-	#frappe.throw(_(customer_filter))
 	columns = [
 		{"fieldname": "sales_order", "label": _("ID"), "fieldtype": "Link", "options": "Sales Order", "width": 100},
 		{"fieldname": "salesman", "label": _("Salesman"), "fieldtype": "Data", "width": 100},
@@ -28,7 +26,6 @@ def execute(filters=None):
 		{"fieldname": "total", "label": _("Total"), "fieldtype": "Currency", "width": 100},
 		{"fieldname": "payment", "label": _("Payment"), "fieldtype": "Currency", "width": 100},
 		{"fieldname": "returns", "label": _("Returns"), "fieldtype": "Currency", "width": 100},
-	#	{"fieldname": "return_payment", _("label"): "Returns Payment", "fieldtype": "Currency", "width": 100},
 		{"fieldname": "pending", "label": _("Pending"), "fieldtype": "Currency", "width": 100},
 		{"fieldname": "status", "label": _("Status"), "fieldtype": "Data", "width": 100},
 		{"fieldname": "delivery_status", "label": _("Delivery"), "fieldtype": "Data", "width": 100},
@@ -76,8 +73,7 @@ def execute(filters=None):
 		inner join `tabSales Order` so
 			on ((gle.against_voucher_type='Sales Invoice')
 			and gle.against_voucher in
-			(select distinct si.parent from `tabSales Invoice Item` si where si.sales_order=so.name )
-		  )
+			(select distinct si.parent from `tabSales Invoice Item` si where si.sales_order=so.name))
 		where voucher_no != against_voucher and gle.is_cancelled = 0 and so.transaction_date between %(fdate)s and %(tdate)s
 		and so.company = %(company)s and so.docstatus=1 and so.status not in ('Closed') {0}
 	""".format(customer_filter), filters, as_dict=1)
@@ -98,9 +94,8 @@ def execute(filters=None):
 			else:
 				so.payment += gle.amount
 
-		so.pending = so.total - so.payment - so.returns  + so.return_payment
-		if so.pending > 1 :
+		so.pending = so.total - so.payment - so.returns + so.return_payment
+		if so.pending > 1:
 			data.append(so)
-
 
 	return columns, data
